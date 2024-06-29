61f6170704209b75ddbee22ca4d7c207a1cb3c61
jerry.buaba <jerry.buaba@mpharma.coma>
2024-06-30 00:00:01 +0100

    adds core files for analyzing required dependencies in generated code and replacing placeholders with gen code

--- src/droplet/core/dependency_analyzer.py
+++ src/droplet/core/dependency_analyzer.py
@@ -1,2 +1,14 @@
+import ast
+from typing import Set
+
 class DependencyAnalyzer:
-    ...
+    def analyze(self, code: str) -> Set[str]:
+        tree = ast.parse(code)
+        imports = set()
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Import):
+                for alias in node.names:
+                    imports.add(alias.name)
+            elif isinstance(node, ast.ImportFrom):
+                imports.add(node.module)
+        return imports
